a42e6864913b85eeea28f4a83c4605f0b9378ff9
abdulsamad1000 <abdulsamad10000@yahoo.com>
2022-08-29 20:55:31 -0500

    Solar System is ready to be deployed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,10 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         // write down your code here
+        SolarSystem inception = new SolarSystem();
+        inception.Sun = 1;
+        inception.Moon =4;
+        inception.Planets = 2;
+        System.out.println("the basic structure is ready");
     }
 }
